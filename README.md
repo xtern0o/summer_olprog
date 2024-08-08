2475abbff3b359f5a45e0e2117dfafb02a948e4a
xtern0o <dos30028@gmail.com>
2024-08-08 23:26:04 +0300

    fdf

--- README.md
+++ README.md
@@ -1,14 +1,8 @@
-# 4 день
-
-## ЕГЭ. Задание 24
-
- > **Необходимый минимум:** строки, срезы, циклы, списочные выражения, операции со списками. 
-
- - Ссылка на [Google Colab](https://colab.research.google.com/drive/1UNlHD5QiG71vWatoV7_z6_k7xsdF5E_K?usp=sharing)
- - Ссылка на [диск с файлами к задачам](https://drive.google.com/drive/folders/1Uew_Tr-_krybbP8QxtOHRCZ11XTl3y5i?usp=sharing)
-  
+# 5 день
 
 ## Графы
+
+> **Необходимый минимум:** списки, двумерные списки, умение ссчитывать двумерные списки, рекурсия, конструкция global (в Python)
 
 ### Ссчитывание графа
  - [От матрицы смежности к списку ребер, неориентированный вариант](https://informatics.msk.ru/mod/statements/view.php?chapterid=464#1)
@@ -24,6 +18,7 @@
  - [Циклы через ребра](https://informatics.msk.ru/mod/statements/view.php?chapterid=112824#1) (задача на [поиск мостов](http://www.e-maxx-ru.1gb.ru/algo/bridge_searching))
 
 ## [Задача с региона](https://informatics.msk.ru/mod/statements/view.php?chapterid=114540#1)
-  
-# я
-![alt text](image.png)
+
+## Зацените моего кота (!)
+
+![alt text](image-1.png)
--- razbienie.cpp
+++ razbienie.cpp
@@ -1,45 +1,47 @@
 #include <iostream>
-#include <cmath>
 #include <algorithm>
-
+#include <cmath>
 
 using namespace std;
 
-
 int main() {
+
     int t; cin >> t;
 
-    for (int _ = 0; _ < t; _++) {
+    while (t--) {
         int n, m; cin >> n >> m;
 
-        double i = (-2 + pow(4 + 2 * (pow(m, 2) + m - 2), 0.5)) / 2;
-        int i1 = floor(i);
-        int i2 = ceil(i);
-        
-        int diff_i1 = abs((i1 + 1) * (i1 + 2) / 2 - (i1 + m + 1) * (m - 1) / 2);
-        int diff_i2 = abs((i2 + 1) * (i2 + 2) / 2 - (i2 + m + 1) * (m - 1) / 2);
+    int a_i = n, b_i = m * n * (n - 1) + n, c_i = (-1) * m * n * (1 + m * n) / 2;
+    int D_i = b_i * b_i - 4 * a_i * c_i;
 
-        int h = diff_i1 < diff_i2 ? i1 : i2;
+    double i = ((-1) * b_i + pow(D_i, 0.5)) / 2 / a_i;
+    int i_1 = floor(i), i_2 = ceil(i);
 
-        int a = m + 1;
-        int b = 1 - 2 * m - n + 2 + m * n;
-        int c = m * n - 2 * n - m * n * n;
-        
-        int D = b * b - 4 * a * c;
-        double j = (-b + pow(D, 0.5)) / 2 / a;
+    int diff_i1 = abs(a_i * i_1 * i_1 + b_i * i_1 + c_i);
+    int diff_i2 = abs(a_i * i_2 * i_2 + b_i * i_2 + c_i);
 
-        int j1 = floor(j);
-        int j2 = ceil(j);
+    int v = (diff_i1 < diff_i2) ? i_1 : i_2;
 
-        int diff_j1 = (1 + m * (j1 - 1)) * j1;
-        int diff_j2 = (j2 + 1 + m * (n - 1) + 1) * (n - j2);
+    
+    int a_j = 2 * m, b_j = m * (m + 1) - m + 1, c_j = (-1) * (1 + m * n) * m * n;
+    int D_j = b_j * b_j - 4 * a_j * c_j;
 
-        int v = diff_j1 < diff_j2 ? j1 : j2;
+    double j = ((-1) * b_j + pow(D_j, 0.5)) / 2 / a_j;
+    int j_1 = floor(j), j_2 = ceil(j);
 
-        if (min(diff_j1, diff_j2) < min(diff_i1, diff_i2)) cout << "H " << v + 1;
-        else cout << "V " << h + 1;
+    int diff_j1 = abs(a_j * j_1 * j_1 + b_j * j_1 + c_j);
+    int diff_j2 = abs(a_j * j_2 * j_2 + b_j * j_2 + c_j);
 
-        cout << endl;
+    int h = (diff_j1 < diff_j2) ? j_1 : j_2;
+
+    if (min(diff_i1, diff_i2) < min(diff_j1, diff_j2)) {
+        cout << "V " << v + 1;
     }
+    else cout << "H "<< h + 1;
+
+    cout << endl;
+
+    }
+    
     
 }
